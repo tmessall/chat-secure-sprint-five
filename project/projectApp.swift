9ffba80a90c9f10234b7f7dcd6a81c97a37a5310
tmessall <tmessall17@gmail.com>
2023-09-29 12:17:15 -0500

    Initial Commit

---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Travis Messall on 9/29/23.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
