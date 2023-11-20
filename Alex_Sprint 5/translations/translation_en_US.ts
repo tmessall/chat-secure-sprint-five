a5c311e186abdbdf308adf50c5f082dcd17eb399
alexanderseelye <122333202+alexanderseelye@users.noreply.github.com>
2023-11-19 21:42:40 -0600

    Choreograph Project to Begin Utilizing Microphone

---
+++ Alex_Sprint 5/translations/translation_en_US.ts
@@ -0,0 +1,120 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Sorry, no word was recognized.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Sorry, no word was recognized.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Im gonna take a break.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Im gonna take a break.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (1)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Awesome! Sounds good.</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Awesome! Sounds good.</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Lets dance!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Lets dance!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (2)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Awe that's too bad. Maybe next time!</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Awe that's too bad. Maybe next time!</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Back double bicep!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Back double bicep!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (3)</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello! What would you like me to do?</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello! What would you like me to do?</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (4)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Sorry, I didn't understand. Could you say it again.</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Sorry, I didn't understand. Could you say it again.</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Animated Say (5)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Ok. I am finished now</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Ok. I am finished now</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Get Localized Text</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello from the ROBOT!!!</source>
+            <comment>Text</comment>
+            <translation type="unfinished">Hello from the ROBOT!!!</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Say</name>
+        <message>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation type="vanished">Hello</translation>
+        </message>
+        <message>
+            <source>Fuck you</source>
+            <comment>Text</comment>
+            <translation type="obsolete">Fuck you</translation>
+        </message>
+    </context>
+</TS>
